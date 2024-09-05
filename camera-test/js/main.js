7a708fd9349f9df5143c4992b654ef5c7e51e8ac
Toan Trieu <toan.trieu@trustingsocial.com>
2024-09-05 17:47:39 +0700

    Update camera test

--- camera-test/js/main.js
+++ camera-test/js/main.js
@@ -1,47 +1,41 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-'use strict';
-
-// Put variables in global scope to make them available to the browser console.
-const constraints = window.constraints = {
+const constraints = (window.constraints = {
   audio: false,
   video: {
-    facingMode: 'user',
-    width: 480,
-    height: 640
-  }
-};
+    facingMode: "user",
+    width: {
+      ideal: 640,
+    },
+  },
+});
 
 function handleSuccess(stream) {
-  const video = document.querySelector('video');
+  const video = document.querySelector("video");
   const videoTracks = stream.getVideoTracks();
-  console.log('Got stream with constraints:', constraints);
+  console.log("Got stream with constraints:", constraints);
   console.log(`Using video device: ${videoTracks[0].label}`);
-  window.stream = stream; // make variable available to browser console
   video.srcObject = stream;
 }
 
 function handleError(error) {
-  if (error.name === 'OverconstrainedError') {
+  if (error.name === "OverconstrainedError") {
     const v = constraints.video;
-    errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
-  } else if (error.name === 'NotAllowedError') {
-    errorMsg('Permissions have not been granted to use your camera and ' +
-      'microphone, you need to allow the page access to your devices in ' +
-      'order for the demo to work.');
+    errorMsg(
+      `The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`
+    );
+  } else if (error.name === "NotAllowedError") {
+    errorMsg(
+      "Permissions have not been granted to use your camera and " +
+        "microphone, you need to allow the page access to your devices in " +
+        "order for the demo to work."
+    );
   }
   errorMsg(`getUserMedia error: ${error.name}`, error);
 }
 
 function errorMsg(msg, error) {
-  const errorElement = document.querySelector('#errorMsg');
+  const errorElement = document.querySelector("#errorMsg");
   errorElement.innerHTML += `<p>${msg}</p>`;
-  if (typeof error !== 'undefined') {
+  if (typeof error !== "undefined") {
     console.error(error);
   }
 }
@@ -56,4 +50,4 @@ async function init(e) {
   }
 }
 
-document.querySelector('#showVideo').addEventListener('click', e => init(e));
+document.querySelector("#showVideo").addEventListener("click", (e) => init(e));
